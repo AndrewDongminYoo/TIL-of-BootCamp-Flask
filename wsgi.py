6ad666565d7d518631766b0d42c4c8d2f7f243cb
AndrewDongminYoo <ydm2790@gmail.com>
2021-11-06 12:02:51 +0900

    scheduler setting

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,8 @@
 from application import application
+from apps.crawller import sched
 
 if __name__ == '__main__':
+    sched.start()
     application.debug = True
     application.env = "development"
     application.run()
